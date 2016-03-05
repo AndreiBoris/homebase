436bb75bba555b96a736a90e33806cd27c98aeea
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2016-03-05 00:15:05 -0500

    Add template for nav items using Backbone

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,63 @@
-'use strict';
+$(function() {
+    /* global Backbone */
+    /* global _ */
+
+    'use strict';
+    var NavItemModel = Backbone.Model.extend({
+        defaults: {
+            title: 'More'
+        }
+    });
+
+    var NavItems = Backbone.Collection.extend({
+        model: NavItemModel
+    });
+
+    var navItems = new NavItems([
+        new NavItemModel({ title: 'Projects' }),
+        new NavItemModel({ title: 'Contact' }),
+        new NavItemModel({ title: 'About Me' }),
+        new NavItemModel({ title: 'Blog' })
+        ]);
+
+    var NavItemView = Backbone.View.extend({
+        tagName: 'li',
+        className: 'navbar__item',
+
+        events: {
+            'click': 'navigate'
+        },
+
+
+        initialize: function(){
+            _.bindAll(this, 'render', 'navigate');
+            this.template = _.template($('#template__navbar__item').html());
+            this.render();
+        },
+
+        render: function() {
+            // Create the HTML
+            this.$el.html(this.template(this.model.attributes));
+            // this.$el.addClass('navbar__item');
+            return this;
+        },
+
+        navigate: function() {
+            console.log('Navigating with' + this.$el.text());
+        }
+    });
+
+    var NavView = Backbone.View.extend({
+        el: '#navigation',
+
+        initialize: function() {
+            navItems.each(function(navItem){
+                var view = new NavItemView({model: navItem});
+                this.$el.append(view.render().el);
+            }, this);
+        },
+
+    });
+
+    var navBar = new NavView();
+});
