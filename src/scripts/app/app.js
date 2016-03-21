622acf19e85323c9bd9cb078fb504a44fca54797
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2016-03-20 21:02:13 -0400

    chore: Make tests run from main require script
    Now all the requireJS scripts originate in scripts/launch.js
    
    The issues with this is that we need some way of easily making sure
    that the tests aren't being run in the production version of this
    website.

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -1,6 +1,12 @@
-/* global require, requirejs */
-
-require([],
-    function() {
+require(['views/home', 'app/router'],
+    function(Home, Router) {
         'use strict';
+        console.log('app.js is running');
+        var home = new Home();
+
+        var router = new Router();
+
+        router.on('route:home', function() {
+            home.render();
+        });
     });
--- src/scripts/launch.js
+++ src/scripts/launch.js
@@ -1,6 +1,7 @@
 (function() {
     'use strict';
     /* global requirejs */
+    console.log('launch.js is running');
 
     requirejs.config({
         paths: {
@@ -10,8 +11,40 @@
             jquery: 'libs/jquery.min',
             underscore: 'libs/underscore-min',
             backbone: 'libs/backbone-min'
-        }
+        },
     });
 
     requirejs(['app/app.min']);
+
+    /**
+     * ALL TEST RELATED SCRIPTS ARE BELOW:
+     */
+
+    requirejs.config({
+        paths: {
+            // All test-related paths
+            tests: 'tests',
+            jasmine: 'libs/test/jasmine',
+            jasminehtml: 'libs/test/jasmine-html',
+            jasmineboot: 'libs/test/boot'
+        },
+        // shim: makes external libraries compatible with requirejs (AMD)
+        shim: {
+            jasminehtml: {
+                deps: ['jasmine']
+            },
+            jasmineboot: {
+                deps: ['jasmine', 'jasminehtml']
+            }
+        }
+    });
+
+    // Run tests
+    require(['jasmineboot'], function() {
+        require(['tests/testrouter'], function() {
+            //trigger Jasmine
+            console.log('go go go');
+            window.onload();
+        });
+    });
 })();
